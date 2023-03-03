0781b929dae447b1feb1ac378209e63a6b582f1c
Byung-ju Kim <airbj31@users.noreply.github.com>
2023-03-03 14:55:28 +0900

    typo error correction

--- R/bioLinkGen.R
+++ R/bioLinkGen.R
@@ -24,25 +24,25 @@ bioLinkGen <- function(x,format="text",target="_blank",class="auto",desc=FALSE) 
   if(class=="auto") {
     class <- case_when(
                     is.na(x)                                                 ~ "None",
-                    substr(x,1,3)=="SRR"                                     ~ "Run",            ## SRA Run
-                    substr(x,1,3)=="SRX"                                     ~ "Experiment",     ## SRA Experiment
-                    substr(x,1,3)=="PRJ"                                     ~ "BioProject",     ## SRA Bioproject
-                    substr(x,1,4)=="SAMN"                                    ~ "biosample",      ## Biosample
-                    substr(x,1,3) %in% c("GCA","GCF")                        ~ "assembly",       ## Assembly accession
-                    substr(x,1,3) %in% c("AC_","NC_","NG_")                  ~ "RefSeq",         ## Genomic molecule for alternative assembly, referencem and incomplete genomic region
-                    substr(x,1,3) %in% c("NT_","NW_")                        ~ "RefSeq",         ## Contig or scaffold.
-                    substr(x,1,3) %in% c("NZ_")                              ~ "RefSeq",         ## Complete genomes and unfinished WGS data
-                    substr(x,1,3) %in% c("NM_","XM_")                        ~ "RefSeq",         ## Protein-coding transcripts for curation and predicted model respectly.
-                    substr(x,1,3) %in% c("NR_","XR_")                        ~ "RefSeq",         ## Non-protein-coding transcripts/	Predicted model non-protein-coding transcript
-                    substr(x,1,3) %in% c("AP_","NP_","YP_","XP_","WP_")      ~ "RefSeq",         ## Annotated on AC_ alternate assembly, Associated with an NM_ or NC_ accession, Annotated on genomic molecules without an instantiated, (Predicted model) associated with an XM_ accession, Non-redundant across multiple strains and species
-                    grepl("^[a-zA-Z]{1}[0-9]{5}",x,perl=TRUE)                ~ "genbank"         ## genbank nucleotides
-                    grepl("^[a-zA-Z]{2}[0-9]{6}",x,perl=TRUE)                ~ "genbank"         ## genbank nucleotides
-                    grepl("^[a-zA-Z]{2}[0-9]{8}",x,perl=TRUE)                ~ "genbank"         ## genbank nucleotides
-                    grepl("^[a-zA-Z]{3}[0-9]{5}",x,perl=TRUE)                ~ "genbank_protein" ## genbank protein
-                    grepl("^[a-zA-Z]{3}[0-9]{7}",x,perl=TRUE)                ~ "genbank_protein" ## genbank protein
-                    grepl("^[a-zA-Z]{4}[0-9]{8+}",x,perl=TRUE)               ~ "genbank_WGS"     ## genbank WGS
-                    grepl("^[a-zA-Z]{6}[0-9]{9+}",x,perl=TRUE)               ~ "genbank_WGS"     ## genbank WGS
-                    grepl("^[a-zA-Z]{5}[0-9]{7}",x,perl=TRUE)                ~ "genbank_MGA"     ## genbank MGA
+                    substr(x,1,3)=="SRR"                                     ~ "Run",             ## SRA Run
+                    substr(x,1,3)=="SRX"                                     ~ "Experiment",      ## SRA Experiment
+                    substr(x,1,3)=="PRJ"                                     ~ "BioProject",      ## SRA Bioproject
+                    substr(x,1,4)=="SAMN"                                    ~ "biosample",       ## Biosample
+                    substr(x,1,3) %in% c("GCA","GCF")                        ~ "assembly",        ## Assembly accession
+                    substr(x,1,3) %in% c("AC_","NC_","NG_")                  ~ "RefSeq",          ## Genomic molecule for alternative assembly, referencem and incomplete genomic region
+                    substr(x,1,3) %in% c("NT_","NW_")                        ~ "RefSeq",          ## Contig or scaffold.
+                    substr(x,1,3) %in% c("NZ_")                              ~ "RefSeq",          ## Complete genomes and unfinished WGS data
+                    substr(x,1,3) %in% c("NM_","XM_")                        ~ "RefSeq",          ## Protein-coding transcripts for curation and predicted model respectly.
+                    substr(x,1,3) %in% c("NR_","XR_")                        ~ "RefSeq",          ## Non-protein-coding transcripts/	Predicted model non-protein-coding transcript
+                    substr(x,1,3) %in% c("AP_","NP_","YP_","XP_","WP_")      ~ "RefSeq",          ## Annotated on AC_ alternate assembly, Associated with an NM_ or NC_ accession, Annotated on genomic molecules without an instantiated, (Predicted model) associated with an XM_ accession, Non-redundant across multiple strains and species
+                    grepl("^[a-zA-Z]{1}[0-9]{5}",x,perl=TRUE)                ~ "genbank",         ## genbank nucleotides
+                    grepl("^[a-zA-Z]{2}[0-9]{6}",x,perl=TRUE)                ~ "genbank",         ## genbank nucleotides
+                    grepl("^[a-zA-Z]{2}[0-9]{8}",x,perl=TRUE)                ~ "genbank",         ## genbank nucleotides
+                    grepl("^[a-zA-Z]{3}[0-9]{5}",x,perl=TRUE)                ~ "genbank_protein", ## genbank protein
+                    grepl("^[a-zA-Z]{3}[0-9]{7}",x,perl=TRUE)                ~ "genbank_protein", ## genbank protein
+                    grepl("^[a-zA-Z]{4}[0-9]{8+}",x,perl=TRUE)               ~ "genbank_WGS",     ## genbank WGS
+                    grepl("^[a-zA-Z]{6}[0-9]{9+}",x,perl=TRUE)               ~ "genbank_WGS",     ## genbank WGS
+                    grepl("^[a-zA-Z]{5}[0-9]{7}",x,perl=TRUE)                ~ "genbank_MGA",     ## genbank MGA
                     grepl("^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$",x,perl=TRUE) ~ "UniprotKB"       ## uniprot
                   )
   }
